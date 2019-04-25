c9be9718397039f2d909378e7b6d8c35809b6575
joao <joao@FX4100-8192MB>
2019-04-25 01:57:22 -0300

    Multiplicação de inteiros Ok, Divisão de inteiros incompleto

--- src/numero/Inteiro.java
+++ src/numero/Inteiro.java
@@ -5,14 +5,26 @@ public class Inteiro{
 	protected int[] inteiro;
 	int numBits;
 	//protected boolean sinal;
-	protected int resto;
+	protected int[] resto;
 	
 	public int getInteiroEm(int indice) {
 		return inteiro[indice];
 	}
 	
-
-	public Inteiro() {
+	protected static Inteiro copiarInteiro(Inteiro A) {
+		return new Inteiro(A);
+	}
+	
+	public Inteiro(Inteiro A) {
+		this.inteiro = new int[A.inteiro.length];
+		for(int i = 0; i < A.inteiro.length; ++i) {
+			this.inteiro[i] = A.inteiro[i];
+		}
+		this.numBits = inteiro.length;
+		this.resto = new int[A.resto.length];
+		for(int i = 0; i < A.resto.length; ++i) {
+			this.resto[i] = A.resto[i];
+		}
 	}
 	
 	public Inteiro(int[] valor) {
@@ -21,9 +33,44 @@ public class Inteiro{
 			this.inteiro[i] = valor[i];
 		}
 		this.numBits = inteiro.length;
-		this.resto = -1;
+		this.resto = new int[inteiro.length];
+	}
+	
+	public Inteiro(int nBits) {
+		this(new int[nBits]);
+	}
+	
+	public static int LSH(int[] valor) {
+		int retorno = valor[valor.length-1];
+		for(int i = 0; i < valor.length-1; ++i) {
+			valor[i+1] = valor[i];
+		}
+		
+		return retorno;
+	}
+	
+	public static int RSH(int[] valor) {
+		int retorno = valor[0];
+		for(int i = 1; i < valor.length; ++i) {
+			valor[i-1] = valor[i];
+		}
+		
+		return retorno;
 	}
 
+	public static int[] nega(int[] valor) {
+		int[] pInvertido = new int[valor.length];
+		int[] pUm = new int[valor.length];
+		pUm[0] = 1;		
+		for(int i = 0; i < valor.length; ++i) {
+			pInvertido[i] = valor[i] == 0 ? 1 : 0;
+		}
+		Inteiro invertido = new Inteiro(pInvertido);
+		Inteiro um = new Inteiro(pUm);
+		return Inteiro.soma(invertido, um).inteiro;
+		
+	}
+	
 	public static Inteiro nega(Inteiro valor) {
 		int[] pInvertido = new int[valor.numBits];
 		int[] pUm = new int[valor.numBits];
@@ -34,17 +81,12 @@ public class Inteiro{
 		}
 		Inteiro invertido = new Inteiro(pInvertido);
 		Inteiro um = new Inteiro(pUm);
-		
 		return Inteiro.soma(invertido, um);
 		
 	}
 
 	public static Inteiro soma(Inteiro valor1, Inteiro valor2) {
 		int nBits = valor1.numBits;
-//		if(valor1.numBits > valor2.numBits)
-//			nBits = valor1.numBits;
-//		else 
-//			nBits = valor2.numBits;
 		boolean sinal = valor1.inteiro[nBits - 1] == valor2.inteiro[nBits - 1];
 		int[] pInteira = new int[nBits];
 		
@@ -76,18 +118,60 @@ public class Inteiro{
 	public static Inteiro subtracao(Inteiro valor1, Inteiro valor2) {
 		return Inteiro.soma(valor1, Inteiro.nega(valor2));
 	}
-	public static Inteiro multiplicacao(Inteiro valor1, Inteiro valor2) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Inteiro multiplicacao(Inteiro Q, Inteiro M) {
+		int numBits = Q.numBits + M.numBits;
+		Inteiro A = new Inteiro(Q.numBits);
+		int Q0 = 0;
+		
+		for(int contador = Q.numBits; contador != 0; --contador) {
+			if(Q.getInteiroEm(0) == 1 && Q0 == 0) {	
+				A = subtracao(A, M);
+			}else if(Q.getInteiroEm(0) == 0 && Q0 == 1) {
+				A = soma(A, M);
+			}			
+			Q0 = RSH(Q.inteiro);
+			Q.inteiro[Q.numBits - 1] = RSH(A.inteiro);
+		}
+		
+		Inteiro resultado = new Inteiro(numBits);
+		
+		for(int i = 0; i < Q.numBits; ++i)
+			resultado.inteiro[i] = Q.inteiro[i];
+		for(int i = Q.numBits; i < numBits; ++i)
+			resultado.inteiro[i] = A.getInteiroEm(i - Q.numBits);
+		return resultado;
 	}
 
-	public static Inteiro divisao(Inteiro valor1, Inteiro valor2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public static Inteiro divisao(Inteiro valor) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Inteiro divisao(Inteiro Q, Inteiro M) {
+		int sinalQ = Q.getInteiroEm(Q.numBits-1);
+		int sinalM = M.getInteiroEm(M.numBits-1);
+		if(sinalQ == 1) Q = nega(Q);
+		if(sinalM == 1) M = nega(M);
+		int qBits = Q.numBits - 1;
+		Inteiro A = new Inteiro(Q.numBits);
+		A.resto = new int[Q.numBits];
+		
+		for(int i = 0; i < Q.numBits ; ++i) {
+			A.resto[i] = Q.getInteiroEm(i);
+		}
+		
+		for(int i = qBits-1; i != 0; --i) {
+			LSH(A.inteiro);
+			A.inteiro[0] = LSH(A.resto);
+			Inteiro restaurar = copiarInteiro(A);
+			A = subtracao(A, M);
+			
+			if(A.getInteiroEm(A.numBits-1) == 1) {
+				A.resto[0] = 0;
+				A = copiarInteiro(restaurar);
+			}
+			else if (A.getInteiroEm(A.numBits-1) == 0) {
+				A.resto[0] = 1;
+			}
+		}		
+		if(sinalQ != sinalM)
+			A.inteiro = nega(A.inteiro);
+		return A;
 	}
 
 	public void print() {
